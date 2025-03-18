2fd47c84e24a4e29e8444a3b1bab2f30cbefde55
Zeenab <zeenab.taher@tuni.fi>
2025-03-18 20:15:51 +0200

    Working program TODO document

--- image_encryption/image_encryption.py
+++ image_encryption/image_encryption.py
@@ -18,7 +18,33 @@ root.geometry("200x160")
 def encrypt_image():
     file1=filedialog.askopenfile(mode='r', filetype=[('jpg file','*.jpg')]) ##TODO add png file
     if file1 is not None:
-        print(file1)
+        ##printing image details to make confirm opening
+        print(file1) 
+        file_name=file1.name
+        print(file_name)
+        
+        ##Confirm the key entered by user before opening image
+        key=entry1.get(1.0,END)
+        print(file_name,key)
+        
+        ##Extract data from the image
+        openFile=open(file_name,'rb')##rb method will read the data in byte format
+        image=openFile.read()
+        openFile.close()
+        
+        ##Converting image data to byte array
+        image=bytearray(image)
+        ##Enumerate method to define the key for each value of image variable
+        for index, values in enumerate(image):
+            ##zor operator to encryp and decrypt
+            ##position of each value and then value itself
+            image[index]=values^int(key) 
+        fi1=open(file_name, 'wb') ##Wb method writing data
+        fi1.write(image)
+        fi1.close()
+        
+        
+        
 
 ##Button to open file dialog box for submit key value
 b1=Button(root,text="encrypt", command=encrypt_image)
